7d48895019f18947dda44fe87025dd81d5f09ab5
Jonghun Park <jonghun.park.194@gmail.com>
2020-09-24 02:23:14 +0900

    Test e2e data flow for future development
    Write e2e data flow in App.vue and background.js for
    future reference.

--- vuel/src/App.vue
+++ vuel/src/App.vue
@@ -1,60 +1,33 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
-    <v-main>
-      <HelloWorld/>
-    </v-main>
+    <v-btn @click="ls"> test </v-btn>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+import { ipcRenderer } from 'electron'
 
 export default {
   name: 'App',
 
   components: {
-    HelloWorld
   },
 
   data: () => ({
     //
-  })
+  }),
+
+  methods: {
+    ls () {
+      console.log('issuing ls')
+      ipcRenderer.send('cmd', 'ls')
+    }
+  },
+
+  mounted () {
+    ipcRenderer.on('cmd-reply', (event, arg) => {
+      console.log('got a reply', arg)
+    })
+  }
 }
 </script>
--- vuel/src/background.js
+++ vuel/src/background.js
@@ -1,3 +1,30 @@
 import { init } from './background_utils'
+import { exec } from 'child_process'
+import { app } from 'electron'
+import { ipcMain } from 'electron'
 
 init()
+console.log('hi')
+
+ipcMain.on('cmd', (event, arg) => {
+  console.log('cmd: ', arg)  // "ping" 출력
+
+  exec("ls -la", (error, stdout, stderr) => {
+    if (error) {
+      console.log(`error: ${error.message}`)
+      return
+    }
+    if (stderr) {
+      console.log(`stderr: ${stderr}`)
+      return
+    }
+
+    console.log('sending: ', stdout)
+    event.sender.send('cmd-reply', stdout)
+  })
+})
+
+ipcMain.on('synchronous-message', (event, arg) => {
+  console.log(arg)  // "ping" 출력
+  event.returnValue = 'pong'
+})
